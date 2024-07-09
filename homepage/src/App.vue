d3662b8a5ce21d4ece169bd4928f758621530baf
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-09 13:40:31 +0900

    change landing page

--- homepage/src/App.vue
+++ homepage/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <header>
-    <div class="container">
-      <nav class="navbar">
-        <div class="navbar_logo">
-          <router-link to="/">
-            <img src="./assets/main_assets/logo-small.png" />
-          </router-link>
-        </div>
-        <ul class="navbar_menu">
-          <li><router-link to="/about">About us</router-link></li>
-          <li><router-link to="/publications">Publications</router-link></li>
-          <li><router-link to="/members">Members</router-link></li>
-        </ul>
-      </nav>
-    </div>
+    <nav class="navbar">
+      <div class="navbar_logo">
+        <router-link to="/">
+          <img src="./assets/main_assets/logo-small.png" />
+        </router-link>
+      </div>
+      <ul class="navbar_menu">
+        <li><router-link to="/about">About us</router-link></li>
+        <li><router-link to="/publications">Publications</router-link></li>
+        <li><router-link to="/members">Members</router-link></li>
+      </ul>
+    </nav>
   </header>
   <router-view/>
   <footer class="footer">
---
+++ homepage/src/components/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="landingpage"></div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
