8115d634ea0400a98dfad3bf2c0872020c3c3531
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-10 17:05:13 +0900

    update css

--- homepage/src/components/LandingPage.vue
+++ homepage/src/components/LandingPage.vue
@@ -53,7 +53,6 @@ export default {
 
       this.map = new kakao.maps.Map(container, options);
       this.displayMarkers()
-      this.changeCSStoMarker()
     },
     getMarkerPositions() {
       return [
@@ -88,20 +87,6 @@ export default {
         this.markers.push(marker)
       })
     },
-    changeCSStoMarker() {
-      var infoTitle = document.querySelectorAll('.info-title');
-      infoTitle.forEach(function(e) {
-          var w = e.offsetWidth + 10;
-          var ml = w/2;
-          e.parentElement.style.top = "82px";
-          e.parentElement.style.left = "50%";
-          e.parentElement.style.marginLeft = -ml+"px";
-          e.parentElement.style.width = w+"px";
-          e.parentElement.previousSibling.style.display = "none";
-          e.parentElement.parentElement.style.border = "0px";
-          e.parentElement.parentElement.style.background = "unset";
-      });
-    }
   }
 }
 </script>
