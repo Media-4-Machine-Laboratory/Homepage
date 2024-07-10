790b0d40d3192671996e4ad88c6e8185f49ef384
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-10 17:01:17 +0900

    update map in landing page

--- homepage/src/components/LandingPage.vue
+++ homepage/src/components/LandingPage.vue
@@ -3,48 +3,43 @@
     <section class="land">
       <h1 class="main-logo">
         <img src="../assets/main_assets/logo-big.png" />
-      </h1>
+      </h1><br>
       <p class="description-logo">
         We are ...
       </p>
     </section>
     <section class="map">
-      <h1> Map here </h1>
-      <div id="map">
+      <div class="row">
+        <div class="col">
+          <h1>MAP</h1>
+        </div>
+        <div class="col">
+          <div id="map"></div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import { toRaw } from "vue"
+//import { toRaw } from "vue"
 
 export default {
   name: 'LandingPage',
   data() {
     return {
-      markerPosition1: [
-        [35.11576734805419, 128.96625334981783]
-      ],
-      markerPosition2: [
-        [35.117131940674106, 128.96768453876615]
-      ],
-      markers: [],
-      infowindow: null
+      markers: []
     }
   },
   mounted() {
     if (window.kakao && window.kakao.maps) {
       this.initMap();
-      this.displayMarker(this.markerPosition1);
-      this.displayMarker(this.markerPosition2);
-      this.displayInfoWindow(this.markerPosition1, "LAB");
     } else {
       const script = document.createElement("script");
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=4e668ae5cd69a64aecd93e568fc960c7";
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=dcf2430e47f4d6e4c7eae6a01045a618";
       document.head.appendChild(script);
     }
   },
@@ -52,58 +47,48 @@ export default {
     initMap() {
       const container = document.getElementById("map");
       const options = {
-        center: new kakao.maps.LatLng(35.11544015468396, 128.967534172774),
-        level: 5,
+        center: new kakao.maps.LatLng(35.11464802405306, 128.96692288109705),
+        level: 4
       };
 
       this.map = new kakao.maps.Map(container, options);
+      this.displayMarkers()
     },
-    changeSize(size) {
-      const container = document.getElementById("map");
-      container.style.width = `${size}px`;
-      container.style.height = `${size}px`;
-      toRaw(this.map).relayout();
+    getMarkerPositions() {
+      return [
+        {title: 'LAB', latlng: new kakao.maps.LatLng(35.11582996167435, 128.96628227368254)},
+        {title: 'Professor Office', latlng: new kakao.maps.LatLng(35.11705318007645, 128.96754006345864)}
+      ]
     },
-    displayMarker(markerPositions) {
+    displayMarkers() {
+      const map = this.map
+      var positions = this.getMarkerPositions()
+
       if (this.markers.length > 0) {
-        this.markers.forEach((marker) => marker.setMap(null));
+        this.markers.forEach((item) => {
+          item.setMap(null);
+        })
       }
 
-      const positions = markerPositions.map(
-        (position) => new kakao.maps.LatLng(...position)
-      );
+      positions.forEach((position) => {
+        const infoWindow = new kakao.maps.InfoWindow({
+          removable: true,
+          content: `<span class="info-title">${position.title}</span>`
+        })
 
-      if (positions.length > 0) {
-        this.markers = positions.map(
-          (position) =>
-            new kakao.maps.Marker({
-              map: toRaw(this.map),
-              position,
-            })
-        );
+        const marker = new kakao.maps.Marker({
+          map,
+          position: position.latlng
+        })
 
-        const bounds = positions.reduce(
-          (bounds, latlng) => bounds.extend(latlng),
-          new kakao.maps.LatLngBounds()
-        );
+        kakao.maps.event.addListener(marker, 'click', () => {infoWindow.open(map, marker)})
+        kakao.maps.event.addListener(marker, 'mouseover', () => {infoWindow.open(map, marker)})
+        kakao.maps.event.addListener(marker, 'mouseout', () => {infoWindow.close(map, marker)})
+        this.markers.push(marker)
+      })
 
-        toRaw(this.map).setBounds(bounds);
-      }
-    },
-    displayInfoWindow(markerPosition, Content) {
-
-      var iwContent = '<div style="padding:5px;">' + Content + '</div>',
-        iwRemoveable = true;
-
-      this.infowindow = new kakao.maps.InfoWindow({
-        map: toRaw(this.map), // 인포윈도우가 표시될 지도
-        position: markerPosition,
-        content: iwContent,
-        removable: iwRemoveable,
-      });
-
-      //toRaw(this.map).setCenter(markerPosition);
-    },
+      
+    }
   }
 }
 </script>
