4bfcad94fdb459ccbbb80a6ebecfe5caa23bdcee
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-12 14:10:13 +0900

    update members page

--- homepage/src/views/MembersView.vue
+++ homepage/src/views/MembersView.vue
@@ -5,16 +5,16 @@
         <div class="members-tabs">
             <ul class="nav nav-tabs nav-justified mb-3" id="members1" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a data-mdb-tab-init class="nav-link active" id="members1-tab1" href="#members1-tabs1" role="tab" aria-controls="members1-tabs1" aria-selected="true" style="color: white; font-weight: bold; font-size: 18px;">Professor</a>
+                    <a data-mdb-tab-init class="nav-link active" id="members1-tab1" href="#members1-tabs1" role="tab" aria-controls="members1-tabs1" aria-selected="true" style="color: black; font-weight: bold; font-size: 18px;">Professor</a>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <a data-mdb-tab-init class="nav-link" id="members1-tab2" href="#members1-tabs2" role="tab" aria-controls="members1-tabs2" aria-selected="false" style="color: white; font-weight: bold; font-size: 18px;">Graduate</a>
+                    <a data-mdb-tab-init class="nav-link" id="members1-tab2" href="#members1-tabs2" role="tab" aria-controls="members1-tabs2" aria-selected="false" style="color: black; font-weight: bold; font-size: 18px;">Graduate</a>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <a data-mdb-tab-init class="nav-link" id="members1-tab3" href="#members1-tabs3" role="tab" aria-controls="members1-tabs3" aria-selected="false" style="color: white; font-weight: bold; font-size: 18px;">Undergraduate</a>
+                    <a data-mdb-tab-init class="nav-link" id="members1-tab3" href="#members1-tabs3" role="tab" aria-controls="members1-tabs3" aria-selected="false" style="color: black; font-weight: bold; font-size: 18px;">Undergraduate</a>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <a data-mdb-tab-init class="nav-link" id="members1-tab4" href="#members1-tabs4" role="tab" aria-controls="members1-tabs4" aria-selected="false" style="color: white; font-weight: bold; font-size: 18px;    ">Alumni</a>
+                    <a data-mdb-tab-init class="nav-link" id="members1-tab4" href="#members1-tabs4" role="tab" aria-controls="members1-tabs4" aria-selected="false" style="color: black; font-weight: bold; font-size: 18px;    ">Alumni</a>
                 </li>
             </ul>
         </div>
@@ -127,6 +127,7 @@ export default {
             };
             members_.push(_member)
         });
+        console.log(members_)
         this.members_ = members_
     }
   }
