636ae47b962dff2c8ed68a1d2860a0d723901ea1
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-12 09:42:35 +0900

    update members page

--- homepage/src/views/MembersView.vue
+++ homepage/src/views/MembersView.vue
@@ -1,11 +1,36 @@
 <template>
     <div class="members">
-
+        <MDBTabs v-model="activeTabId3">
+            <!-- Tabs navs -->
+            <MDBTabNav justify tabsClasses="mb-3">
+                <MDBTabItem tabId="members1" href="members1">Professor</MDBTabItem>
+                <MDBTabItem tabId="members2" href="members2">Graduate</MDBTabItem>
+                <MDBTabItem tabId="members3" href="members3">Undergraduate</MDBTabItem>
+                <MDBTabItem tabId="members4" href="members4">Alumni</MDBTabItem>
+            </MDBTabNav>
+        </MDBTabs>
     </div>
 </template>
 
 <script>
+import { MDBTabs, MDBTabNav, MDBTabContent, MDBTabItem, MDBTabPane } from 'mdb-ui-kit';
+import { ref } from 'vue';
+
 export default {
-  name: 'MembersView'
+  name: 'MembersView',
+  components: {
+    MDBTabs,
+    MDBTabNav,
+    MDBTabContent, 
+    MDBTabItem, 
+    MDBTabPane
+  },
+  setup() {
+    const activeTabId3 = ref('members1');
+
+    return {
+        activeTabId3
+    }
+  }
 }
 </script>
