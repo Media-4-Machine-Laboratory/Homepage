aa6a5e8533eb27a301f2349ab7e0d18ae32b39a9
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-18 17:44:16 +0900

    update css and package

--- homepage/src/views/AboutView.vue
+++ homepage/src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <div class="about-landing"></div>
+    <section class="about-landing"  >
+      <div style="align-items: center;">
+        <img class="main-logo" src="../assets/main_assets/logo-big.png" />
+      </div>
+    </section>
+    <section class="about-timeline">
+      <ul class="timeline-events"></ul>
+      <ul class="timeline-years">
+        <li>2023.05.</li>
+        <li></li>
+      </ul>
+    </section>
   </div>
 </template>
 
