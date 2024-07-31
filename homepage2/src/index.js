dfdcf4349a6b27c8fbd3305a327fed74ec904848
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-31 13:12:53 +0900

    update mongodb connect

--- homepage2/src/index.js
+++ homepage2/src/index.js
@@ -1,13 +1,36 @@
 var express = require('express')
-var router = express.Router()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var path = require('path')
+
+require('dotenv').config()
 
 var app = express()
+var port = process.env.PORT || 3000
+var MONGO_URI = process.env.MONGOURI
 
-router.get('/', (req, res) => {
+// parser
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
+// view engine
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+// connect mongodb
+mongoose.connect(
+    MONGO_URI
+).then(() => {
+    console.log('Successfully Connected')
+}).catch((e) => {
+    console.error(e)
 })
 
-app.use('/', router)
-app.listen(3000, function() {
-    console.log('express server on port 3000')
+// router
+app.use('/', require('./routes/index'))
+
+// listen
+app.listen(port, () => {
+    console.log(`express server on port ${port}`)
 })
